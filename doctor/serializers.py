6c996fd18070a53852a18121d87dd70f175827ed
Tanveer Hossain Munim <munim987654321@gmail.com>
2022-11-17 01:39:13 +0600

    doctor Update Done

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -26,3 +26,22 @@ class DoctorSerializer(ModelSerializer):
         doctor, created = Doctor.objects.update_or_create(user=user, designation=validated_data.pop('designation'),
          phone=validated_data.pop('phone'), institute=validated_data.pop('institute'))
         return doctor
+    
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+        user = instance.user
+        user.username = user_data.get('username', user.username)
+        user.email = user_data.get('email', user.email)
+        user.set_password(user_data.get('password', user.password))
+        user.first_name = user_data.get('first_name', user.first_name)
+        user.last_name = user_data.get('last_name', user.last_name)
+        user.save()
+        instance.designation = validated_data.get('designation', instance.designation)
+        instance.institute = validated_data.get('institute', instance.institute)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.address = validated_data.get('address', instance.address)
+        instance.speciality = validated_data.get('speciality', instance.speciality)
+        instance.experience = validated_data.get('experience', instance.experience)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+        return instance
